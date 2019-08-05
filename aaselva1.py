e4f44229fe65ea62611be87b11380c9a86984734
bala977 <52271346+bala977@users.noreply.github.com>
2019-08-05 14:12:44 +0530

    Create aaselva1.py

---
+++ aaselva1.py
@@ -0,0 +1,17 @@
+#B
+n=int(input())
+l=list(map(int,input().split()))
+r=[]
+f=""
+for i in l:
+    if(l.count(i)>1):
+        r.append(i)
+if(len(r)==0):
+    print("unique")
+else:
+    s=set(r)
+    s=list(s)
+    s=sorted(s)
+    for i in s:
+        f=f+str(i)+" "
+    print(f.strip())
