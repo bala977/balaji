f59cd003d2399a5562d988d8d3756d79af934307
bala977 <52271346+bala977@users.noreply.github.com>
2019-08-05 14:25:36 +0530

    Create aaaaaaaaaaaaaaaaa.py

---
+++ aaaaaaaaaaaaaaaaa.py
@@ -0,0 +1,11 @@
+#B
+n=int(input ()) 
+m=list(map(int, input ().split()))
+x=[]
+for i in range(0,n):
+	if i==m[i]:
+		x.append(i)
+for i in range(0,len(x)):
+	print(x[i],end=" ")
+if len(x)==0:
+	print("-1")
