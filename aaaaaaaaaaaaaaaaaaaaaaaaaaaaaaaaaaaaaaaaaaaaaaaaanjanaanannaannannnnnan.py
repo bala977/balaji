fd1bb962ff61cd572b678e958a4fb3a442be3eda
bala977 <52271346+bala977@users.noreply.github.com>
2019-08-05 14:35:01 +0530

    Create aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaanjanaanannaannannnnnan.py

---
+++ aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaanjanaanannaannannnnnan.py
@@ -0,0 +1,11 @@
+#B
+n=int(input())
+m=list(map(int,input().split()))
+s=[]
+for i in range(0,n):
+    if i%2==0 and m[i]%2!=0:
+        s.append(m[i])
+    elif i%2!=0 and m[i]%2==0:
+        s.append(m[i])
+for i in range (0, len(s)): 
+    print (s[i],end=" ")
